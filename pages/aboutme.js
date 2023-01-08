8fd5d17df9625e88b93924b684ea4dd6451da6fd
Makosai <kristopher.ali.dev@gmail.com>
2023-01-07 23:01:01 -0500

    Home page update.

---
+++ pages/aboutme.js
@@ -0,0 +1,12 @@
+import { Background } from '../components/background';
+import MainContainer from '../components/main_container';
+
+export default function AboutMe() {
+  return (
+    <>
+      <MainContainer>
+        <Background>Hello world.</Background>
+      </MainContainer>
+    </>
+  );
+}
