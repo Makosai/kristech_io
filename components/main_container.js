a75f1ec61b2e41b52a41314a8879f01d2ba06377
Makosai <kristopher.ali.dev@gmail.com>
2023-01-10 23:29:38 -0500

    Add animation to front page.

--- components/main_container.js
+++ components/main_container.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function MainContainer({ children }) {
   return (
@@ -12,7 +13,10 @@ export default function MainContainer({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {children}
+      <div className={styles.background}>
+        <span className={styles.background_dot}></span>
+        {children}
+      </div>
     </>
   );
 }
