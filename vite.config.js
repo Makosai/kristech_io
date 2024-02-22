a2fd27d19750e87d24bf0803c0aaef1734d6aaba
Makosai <kristopher.ali.dev@gmail.com>
2024-02-22 14:27:26 -0500

    Setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss({
+	plugins: [enhancedImages(), sveltekit(), purgeCss({
 			safelist: {
 				// any selectors that begin with "hljs-" will not be purged
 				greedy: [/^hljs-/],
