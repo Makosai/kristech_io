a40bed3514777fff7ab5497e7f6f4de2f2d6dc49
Makosai <kristopher.ali.dev@gmail.com>
2024-04-11 15:36:26 -0400

    Add an error screen.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="h-[100vh] w-full flex justify-center items-center">
+	<h1>{$page.status}: {$page.error?.message ?? 'Error'}</h1>
+</div>
